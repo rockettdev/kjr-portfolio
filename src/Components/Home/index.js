1af354f70cfa91ebe8d025eb954f12b3289cadea
Korey <kjr.dev@hotmail.com>
2024-01-12 11:34:48 -0500

    home button css and transition

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -4,7 +4,7 @@ function Home() {
         <>
             <div className="text-neon-yellow"> K J R    D e v    P o r t f o l i o </div>
             <br></br>
-            <button>Continue</button>
+            <button className="text-white hover:text-white border border-neon-yellow hover:scale-105 transform transition duration-80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"> Continue</button>
         </>
     )
 
