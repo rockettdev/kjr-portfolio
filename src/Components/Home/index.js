8d297a618cb369f0d7317fde997107365e10e648
Korey <kjr.dev@hotmail.com>
2024-01-16 13:19:11 -0500

    typewriter animation beta

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -2,7 +2,7 @@ function Home() {
 
     return (
         <>
-            <div className="text-neon-yellow"> K J R    D e v    P o r t f o l i o </div>
+            <div className="text-neon-yellow font-saira tracking-widest text-4xl relative w-[max-content] before:absolute before:inset-0 before:bg-default before:animate-typewriter">KJR Dev Portfolio</div>
             <br></br>
             <button className="text-white hover:text-white border border-neon-yellow hover:scale-105 transform transition duration-80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"> Continue</button>
         </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,23 @@ module.exports = {
     extend: {
       colors: {
         'neon-yellow':'#a4f542',
+        'default':'#282c34'
       },
       fontFamily: {
-        saira: ["Saira"]
+        saira: ["Saira Condensed", "sans-serif"]
+      },
+      letterSpacing: {
+        widest: '.25em'
+      },
+      animation: {
+        typewriter: "typewriter 2s steps(11) forwards"
+      },
+      keyframes: {
+        typewriter: {
+          to: {
+            left: "100%"
+          }
+        }
       }
     },
   },
