aadc36fba4834814dc31ecb751760a67f5360c1d
Korey <kjr.dev@hotmail.com>
2024-01-17 17:28:56 -0500

    Initialised Main Portfolio Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 // import Navbar from './Components/Navbar';
 import Home from './Components/Home';
+import Portfolio from './Components/Portfolio';
 
 function App() {
   return (
     <>
-          <Home/>
+      <Portfolio/>
     </>
   );
 }
---
+++ src/Components/Portfolio/index.js
@@ -0,0 +1,19 @@
+function Portfolio() {
+
+    return (
+        <>
+            <div className="grid md:grid-cols-7 p-4 bg-default items-center">
+                <h1 className="text-3xl text-neon-yellow bg-secondary
+                "> KJR Dev </h1>
+                <h1 className="text-xl mx-auto text-white bg-secondary text-align hover:scale-105
+                "> Work </h1>
+                <h1 className="text-xl mx-auto text-white bg-secondary text-align hover:scale-105
+                "> About Me </h1>
+                <h1 className="text-xl mx-auto text-white bg-secondary text-align hover:scale-105
+                "> Contact Us </h1>
+            </div>
+        </>
+    )
+}
+
+export default Portfolio
