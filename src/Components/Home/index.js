3bc52426dfc28262a6f6d842b6e4d8e00534f1cf
Korey <kjr.dev@hotmail.com>
2024-01-20 20:54:51 -0500

    Logo Changes

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -3,14 +3,14 @@ function Portfolio() {
     return (
         <>
             <div className="grid md:grid-cols-9 p-5 bg-default items-center">
-                <h1 className="font-saira text-4xl text-neon-yellow
-                "> KJR Dev </h1>
-                <h1 className="font-apple text-xl mx-auto text-white text-align hover:scale-105
-                "> Projects </h1>
-                <h1 className="font-apple text-xl mx-auto text-white text-align hover:scale-105
-                "> About Me </h1>
-                <h1 className="font-apple text-xl mx-auto text-white text-align hover:scale-105
-                "> Contact Me </h1>
+                <h1 className="font-apple text-3xl text-neon-yellow
+                ">KJR Dev</h1>
+                <h1 className="font-apple text-l mx-auto text-white text-align hover:scale-105
+                "> PROJECTS </h1>
+                <h1 className="font-apple text-l mx-auto text-white text-align hover:scale-105
+                "> ABOUT ME </h1>
+                <h1 className="font-apple text-l mx-auto text-white text-align hover:scale-105
+                "> CONTACT ME </h1>
             </div>
             <div className="bg-secondary">
                 shush
