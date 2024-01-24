635bf8db7191ec3d836ec6c51393bb380599fa11
Korey <kjr.dev@hotmail.com>
2024-01-24 17:12:12 -0500

    Portfolio Screen CSS changes

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -2,7 +2,7 @@ function Portfolio() {
 
     return (
         <>
-            <div className="grid md:grid-cols-9 p-5 bg-default items-center">
+            <div className="grid md:grid-cols-9 p-5 mt--2 bg-default items-center">
                 <h1 className="font-apple text-3xl text-neon-yellow
                 ">KJR Dev</h1>
                 <h1 className="font-apple text-l mx-auto text-white text-align hover:scale-105
@@ -12,8 +12,8 @@ function Portfolio() {
                 <h1 className="font-apple text-l mx-auto text-white text-align hover:scale-105
                 "> CONTACT ME</h1>
             </div>
-            <div className="bg-secondary">
-                shush
+            <div className="bg-secondary h-screen">
+                <div className="">Shush</div>
             </div>
         </>
     )
