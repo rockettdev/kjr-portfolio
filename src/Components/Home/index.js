7e1846b7128ee7655a86bbd150d7a646380bc9e4
Korey <kjr.dev@hotmail.com>
2024-01-24 17:46:29 -0500

    Updated Grid Layout for Portfolio

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -12,8 +12,14 @@ function Portfolio() {
                 <h1 className="font-apple text-l mx-auto text-white text-align hover:scale-105
                 "> CONTACT ME</h1>
             </div>
-            <div className="bg-secondary h-screen">
-                <div className="">Projects</div>
+            <div className="bg-secondary h-auto">
+                    <h1 className="font-apple pt-32 pb-20 text-center text-xl"> MY PROJECTS </h1>
+                    <div className="grid grid-cols-3 gap-4 place-items-center">
+                        <div className="bg-neon-yellow w-72 h-80">1</div>
+                        <div className="bg-neon-yellow w-72 h-80">2</div>
+                        <div className="bg-neon-yellow w-72 h-80"> 3</div>
+                        <div className="bg-neon-yellow w-72 h-80 mb-40">4</div>
+                    </div>
             </div>
         </>
     )
