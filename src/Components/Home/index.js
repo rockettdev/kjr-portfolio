0a855550fe16c29cab606777737f7c804fe70729
Korey <kjr.dev@hotmail.com>
2024-01-24 17:19:59 -0500

    Project Page Changes

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -2,7 +2,7 @@ function Portfolio() {
 
     return (
         <>
-            <div className="grid md:grid-cols-9 p-5 mt--2 bg-default items-center">
+            <div className="grid md:grid-cols-9 p-5 bg-default items-center">
                 <h1 className="font-apple text-3xl text-neon-yellow
                 ">KJR Dev</h1>
                 <h1 className="font-apple text-l mx-auto text-white text-align hover:scale-105
@@ -13,7 +13,7 @@ function Portfolio() {
                 "> CONTACT ME</h1>
             </div>
             <div className="bg-secondary h-screen">
-                <div className="">Shush</div>
+                <div className="">Projects</div>
             </div>
         </>
     )
