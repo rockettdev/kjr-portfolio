497d2185c9f120176bf6e231f97165ec08657c1f
Korey <kjr.dev@hotmail.com>
2024-01-28 12:16:17 -0500

    Hover States added to grid tiles & CSS Changes

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -15,20 +15,20 @@ function Portfolio() {
             <div className="bg-secondary h-auto">
                     <h1 className="font-apple pt-20 pb-20 text-center text-xl"> MY PROJECTS </h1>
                     <div className="grid grid-cols-3 gap-4 place-items-center">
-                        <div className="bg-neon-yellow w-72 h-72 ml-32 text-center rounded-xl">
-                            <h1> Mini-Golf </h1>
+                        <div className="bg-neon-yellow w-72 h-72 ml-32 text-center rounded-xl hover:scale-105">
+                            <h1 className="text-lg pt-2"> Mini-Golf Tracker </h1>
                         </div>
-                        <div className="bg-neon-yellow w-72 h-72 text-center rounded-xl">
-                            <h1> Mini-Golf</h1>
+                        <div className="bg-neon-yellow w-72 h-72 text-center rounded-xl hover:scale-105">
+                            <h1 className="text-lg pt-2"> Personal Portfolio </h1>
                         </div>
-                        <div className="bg-neon-yellow w-72 h-72 mr-32 text-center rounded-xl">
-                            <h1> Mini-Golf </h1>
+                        <div className="bg-neon-yellow w-72 h-72 mr-32 text-center rounded-xl hover:scale-105">
+                            <h1 className="text-lg pt-2"> FIFA Career Mode Stat Tracker </h1>
                         </div>
-                        <div className="bg-neon-yellow w-72 h-72 mb-64 mt-10 ml-32 text-center rounded-xl">
-                            <h1> Mini-Golf </h1>
+                        <div className="bg-neon-yellow w-72 h-72 mb-64 mt-10 ml-32 text-center rounded-xl hover:scale-105">
+                            <h1 className="text-lg pt-2"> Spotify Frontend Clone </h1>
                         </div>
-                        <div className="bg-neon-yellow w-72 h-72 mb-64 mt-10 text-center rounded-xl mt--20">
-                            <h1> Mini-Golf</h1>
+                        <div className="bg-neon-yellow w-72 h-72 mb-64 mt-10 text-center rounded-xl hover:scale-105">
+                            <h1 className="text-lg pt-2"> Interactive Grocery Basket Calculator </h1>
                         </div>
  
                     </div>
