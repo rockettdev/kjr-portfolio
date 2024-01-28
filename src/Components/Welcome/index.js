b636e7774738f0f985a3ce1cf8d48af7e6ca5edd
Korey <kjr.dev@hotmail.com>
2024-01-28 16:43:34 -0500

    Animation Bug Fixes

--- src/Components/Welcome/index.js
+++ src/Components/Welcome/index.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import '../../App.css';
+import Typewriter from '../Typewriter/index.js'
 
 function Home() {
 
@@ -7,10 +8,14 @@ function Home() {
         <>
             <div className="App">
                 <header className="App-header">
-                    <div className="text-neon-yellow font-saira tracking-widest text-4xl relative w-[max-content] before:absolute before:inset-0 before:bg-default before:animate-typewriter">KJR Dev Portfolio</div>
+                    <div className="text-neon-yellow font-saira text-4xl">
+                        <Typewriter/>
+                    </div>
                     <br></br>
                     <button className="text-white hover:text-white border border-neon-yellow hover:scale-105 transform transition duration-80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"> <Link to="home"> Continue </Link> </button>
                 </header>
+                <video src={process.env.PUBLIC_URL + "/assets/video/nyc.mp4"}
+                loop autoPlay muted> </video>
             </div>
         </>
     )
