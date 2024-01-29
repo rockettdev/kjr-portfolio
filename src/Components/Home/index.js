8b8629c9f09259563a138a9fd3055d05e6077a55
Korey <kjr.dev@hotmail.com>
2024-01-29 17:38:18 -0500

    background video asset loaded

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,7 +1,19 @@
 function About () {
 
 
-    return
+    return (
+        <>
+            <div>
+                <header className="flex flex-col h-screen justify-center items-center">
+                    <div className="text-neon-yellow font-saira text-4xl text-center">
+                    </div>
+                    {/* <button className="text-white hover:text-white border border-neon-yellow hover:scale-105 transform transition duration-80 font-medium rounded-lg text-sm p-2 mt-5" > <Link to="portfolio"> Continue </Link> </button> */}
+                </header>
+                <video src={process.env.PUBLIC_URL + "/assets/video/nyc.mp4"}
+                loop autoPlay muted className="object-cover absolute h-screen w-screen -z-10 top-0 left-0"> </video>
+            </div>
+        </>
+    )
 }
 
 export default About
