93c133ad7c80546555f770b28a849c681a434236
Korey <kjr.dev@hotmail.com>
2024-01-29 14:34:39 -0500

    Code Cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom"
-import './App.css';
 import Welcome from './Components/Welcome';
 import Home from './Components/Home/index.js';
 
--- src/Components/Welcome/index.js
+++ src/Components/Welcome/index.js
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import '../../App.css';
 import Typewriter from '../Typewriter/index.js'
 
 function Home() {
