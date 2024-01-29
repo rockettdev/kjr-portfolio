01bf179c72baf3d08de75b50a1b47d941df0ab89
Korey <kjr.dev@hotmail.com>
2024-01-29 14:17:32 -0500

    backdrop video compression (file reduction)

--- src/Components/Welcome/index.js
+++ src/Components/Welcome/index.js
@@ -12,7 +12,6 @@ function Home() {
                         <Typewriter/>
                     </div>
                     <button className="text-white hover:text-white border border-neon-yellow hover:scale-105 transform transition duration-80 font-medium rounded-lg text-sm p-2 mt-5" > <Link to="home"> Continue </Link> </button>
-                    
                 </header>
                 <video src={process.env.PUBLIC_URL + "/assets/video/nyc.mp4"}
                 loop autoPlay muted className="object-cover absolute h-screen w-screen -z-10 top-0 left-0"> </video>
