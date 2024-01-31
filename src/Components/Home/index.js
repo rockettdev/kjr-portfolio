2b2a7ada594c53237fa150827a4f310ad898c754
Korey <kjr.dev@hotmail.com>
2024-01-31 17:39:39 -0500

    Navbar Component Link Changes

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -11,7 +11,6 @@ function About () {
                     <div className="text-neon-yellow font-saira text-4xl text-center">
                         <h1> Shush </h1>
                     </div>
-                    {/* <button className="text-white hover:text-white border border-neon-yellow hover:scale-105 transform transition duration-80 font-medium rounded-lg text-sm p-2 mt-5" > <Link to="portfolio"> Continue </Link> </button> */}
                 </header>
                 <video src={process.env.PUBLIC_URL + "/assets/video/night.mp4"}
                 loop autoPlay muted className="object-cover absolute h-screen w-screen -z-10 top-0 left-0"> </video>
--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
 
     return (
         <div className="grid md:grid-cols-9 p-5 bg-default items-center">
             <h1 className="font-apple text-3xl text-neon-yellow
-            ">KJR Dev</h1>
+            "> <Link to="/home"> KJR Dev </Link></h1>
             <h1 className="font-apple text-l mx-auto text-white text-align hover:scale-105
-            ">PROJECTS</h1>
+            "> <Link to="/portfolio">PROJECTS</Link></h1>
             <h1 className="font-apple text-l mx-auto text-white text-align hover:scale-105
             "> ABOUT ME </h1>
             <h1 className="font-apple text-l mx-auto text-white text-align hover:scale-105
