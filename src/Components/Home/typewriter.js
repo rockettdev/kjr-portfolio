84f3ce0f6129af6a1a46504208fa82dca9f7c853
Korey <kjr.dev@hotmail.com>
2024-02-06 00:04:10 -0500

    Individual Typewriter for About Page

---
+++ src/Components/Home/typewriter.js
@@ -0,0 +1,26 @@
+import { TypeAnimation } from 'react-type-animation';
+
+const Typewriter = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        `I'm an independent Junior Software Developer from Bath, UK.`,
+        1000
+        // 1000, // wait 1s before replacing "Mice" with "Hamsters"
+        // 'We produce food for Hamsters',
+        // 1000,
+        // 'We produce food for Guinea Pigs',
+        // 1000,
+        // 'We produce food for Chinchillas',
+        // 1000
+      ]}
+      wrapper="span"
+      speed={25}
+      style={{ fontSize: '2em', display: 'inline-block' }}
+      repeat={Infinity}
+    />
+  );
+};
+
+export default Typewriter
