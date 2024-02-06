7bb4f76ad199c232187e85ed908deb2497b7f9e9
Korey <kjr.dev@hotmail.com>
2024-02-06 00:12:55 -0500

    code cleanup

--- src/Components/Home/typewriter.js
+++ src/Components/Home/typewriter.js
@@ -4,16 +4,10 @@ const Typewriter = () => {
   return (
     <TypeAnimation
       sequence={[
-        // Same substring at the start will only be typed out once, initially
+        // Initial substring
         `I'm an independent Junior Software Developer from Bath, UK.`,
         1000
-        // 1000, // wait 1s before replacing "Mice" with "Hamsters"
-        // 'We produce food for Hamsters',
-        // 1000,
-        // 'We produce food for Guinea Pigs',
-        // 1000,
-        // 'We produce food for Chinchillas',
-        // 1000
+        // 1000, // wait 1s before replacing characters
       ]}
       wrapper="span"
       speed={25}
