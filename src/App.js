4882584a84eae40f03802a25fb66e36aab2499f8
Korey <kjr.dev@hotmail.com>
2024-02-12 17:58:53 +0000

    home page removal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom"
 import './App.css';
-import Welcome from './Components/Welcome';
 import Home from './Components/Home/index.js';
 import Portfolio from './Components/Portfolio/index.js';
 
@@ -8,15 +7,11 @@ function App() {
   return (
       <Routes>
           <Route
-          path='/'
-          element={<Welcome />}
-          />
-          <Route
           path='portfolio'
           element={<Portfolio />}
           />
           <Route
-          path='home'
+          path='/'
           element={<Home />}
           />
       </Routes>
